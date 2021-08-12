75a12f2163fadb5d738ce0ae34ea4a1c3d9cb998
nin-jin <nin-jin@users.noreply.github.com>
2021-08-12 07:05:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1099,9 +1099,6 @@ declare namespace $ {
     }
 }
 
-declare namespace $ {
-}
-
 declare namespace $.$$ {
     class $mol_lights_toggle extends $.$mol_lights_toggle {
         lights(next?: boolean): boolean;
