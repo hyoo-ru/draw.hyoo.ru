65b357f1ff63244715abe17d1a89a905749fc595
jin <jin@hyoo.ru>
2021-08-31 01:47:43 +0300

    More colors

--- colors/colors.view.ts
+++ colors/colors.view.ts
@@ -3,27 +3,14 @@ namespace $.$$ {
 	export class $hyoo_draw_colors extends $.$hyoo_draw_colors {
 		
 		@ $mol_mem
-		rows() {
-			return this.options().map( id => this.Option( id ) )
-		}
-		
-		@ $mol_mem
-		color( next?: string ) {
-			return next ?? this.options()[ Math.floor( Math.random() * this.options().length ) ]
+		value( next?: string ) {
+			return next ?? this.keys()[ Math.floor( Math.random() * this.keys().length ) ]
 		}
 		
 		color_raw( id: string ) {
 			return `var(--hyoo_draw_palette_${id})`
 		}
 		
-		option_select( id: string ) {
-			this.color( id )
-		}
-
-		option_title( id: string ) {
-			return id === this.color() ? '✔' : ''
-		}
-
 	}
 
 }
