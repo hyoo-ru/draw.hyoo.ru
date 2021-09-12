3acb01c36784f1986f26efbe10bb2c938fdb9ca1
jin <jin@hyoo.ru>
2021-09-12 14:56:20 +0300

    Hints for tools

--- tools/tools.view.ts
+++ tools/tools.view.ts
@@ -6,11 +6,22 @@ namespace $.$$ {
 		option_label( id: string ) {
 			
 			switch( id ) {
-				case 'move': return [ this.Icon_move() ]
 				case 'pencil': return [ this.Icon_pencil() ]
 				case 'filler': return [ this.Icon_filler() ]
 				case 'eraser': return [ this.Icon_eraser() ]
 				default: return []
+			}
+			
+		}
+		
+		@ $mol_mem_key
+		option_hint( id: string ) {
+			
+			switch( id ) {
+				case 'pencil': return this.hint_pencil()
+				case 'filler': return this.hint_filler()
+				case 'eraser': return this.hint_eraser()
+				default: return ''
 			}
 			
 		}
