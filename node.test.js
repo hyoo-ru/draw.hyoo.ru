ec55f3b5cd5721edf5b731eaac12b16143b02e2f
nin-jin <nin-jin@users.noreply.github.com>
2023-06-05 07:10:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -8106,6 +8106,7 @@ var $;
                 id: $mol_const(knight.id),
                 peer: $mol_const(knight),
             });
+            land_outer.join();
             for (const peer of law)
                 land_outer.level(peer || this.peer.id, $hyoo_crowd_peer_level.law);
             for (const peer of mod)
