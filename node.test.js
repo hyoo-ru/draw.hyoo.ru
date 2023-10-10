f1136cad7355f0a0410f64f43335f96ffaf5e347
nin-jin <nin-jin@users.noreply.github.com>
2023-10-10 07:09:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -10263,6 +10263,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
