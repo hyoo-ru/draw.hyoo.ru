db9c5c70967716d71e59b74be94687f38f44c4d3
nin-jin <nin-jin@users.noreply.github.com>
2023-12-18 07:09:34 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2732,6 +2732,7 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
+        relation(): string;
         event_click(event?: any): any;
         click(event?: any): any;
     }
